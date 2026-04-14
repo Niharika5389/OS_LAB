8015fa298d262103258bafb58ae7ccfa2a83b5ae
Niks05 <81358115+Niharika5389@users.noreply.github.com>
2026-04-14 08:40:30 +0530

    Update q2.c

--- Lab8/q2.c
+++ Lab8/q2.c
@@ -4,116 +4,103 @@
 #define MAXP 20
 #define MAXR 3
 
-bool isSafe(int n, int allocation[MAXP][MAXR], int need[MAXP][MAXR], int available[MAXR]) {
+bool detectDeadlock(int n, int allocation[MAXP][MAXR], int request[MAXP][MAXR], int available[MAXR]) {
     int work[MAXR], count = 0;
     bool finish[MAXP] = {false};
 
-    for (int j = 0; j < MAXR; j++)
+    for(int j = 0; j < MAXR; j++)
         work[j] = available[j];
 
-    while (count < n) {
+    while(count < n) {
         bool found = false;
-        for (int i = 0; i < n; i++) {
-            if (!finish[i]) {
+
+        for(int i = 0; i < n; i++) {
+            if(!finish[i]) {
                 int j;
-                for (j = 0; j < MAXR; j++)
-                    if (need[i][j] > work[j])
+
+                for(j = 0; j < MAXR; j++)
+                    if(request[i][j] > work[j])
                         break;
-                if (j == MAXR) {
-                    for (int k = 0; k < MAXR; k++)
+
+                if(j == MAXR) {
+                    for(int k = 0; k < MAXR; k++)
                         work[k] += allocation[i][k];
+
                     finish[i] = true;
                     count++;
                     found = true;
                 }
             }
         }
-        if (!found)
-            return false;
+
+        if(!found)
+            break;  
     }
-    return true;
+
+    bool deadlock = false;
+    printf("\nDeadlocked processes: ");
+
+    for(int i = 0; i < n; i++) {
+        if(!finish[i]) {
+            printf("P%d ", i);
+            deadlock = true;
+        }
+    }
+
+    if(!deadlock)
+        printf("None");
+
+    printf("\n");
+
+    return deadlock;
 }
 
 int main() {
-    int n, m = MAXR;
-    int allocation[MAXP][MAXR], max[MAXP][MAXR], need[MAXP][MAXR], available[MAXR];
-    int total_resources[MAXR] = {10, 5, 7};
+    int n;
+    int allocation[MAXP][MAXR], request[MAXP][MAXR], available[MAXR];
+    int total[MAXR] = {10, 5, 7};
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    printf("Enter Allocation Matrix:\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
+    printf("\nEnter Allocation Matrix:\n");
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < MAXR; j++)
             scanf("%d", &allocation[i][j]);
 
-    printf("Enter Max Matrix:\n");
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            scanf("%d", &max[i][j]);
+    printf("\nEnter Request Matrix:\n");
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < MAXR; j++)
+            scanf("%d", &request[i][j]);
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            need[i][j] = max[i][j] - allocation[i][j];
-
-    for (int j = 0; j < m; j++) {
-        int sum_alloc = 0;
-        for (int i = 0; i < n; i++)
-            sum_alloc += allocation[i][j];
-        available[j] = total_resources[j] - sum_alloc;
+    for(int j = 0; j < MAXR; j++) {
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+            sum += allocation[i][j];
+        available[j] = total[j] - sum;
     }
 
-    if (!isSafe(n, allocation, need, available)) {
-        printf("System is NOT initially safe.\n");
-        return 0;
-    }
-    printf("System is initially SAFE.\n");
+    printf("\nRunning Deadlock Detection...\n");
 
-    int p, request[MAXR];
-    printf("Enter process number making request: ");
-    scanf("%d", &p);
-    printf("Enter request vector (A B C): ");
-    for (int j = 0; j < m; j++)
-        scanf("%d", &request[j]);
-
-    for (int j = 0; j < m; j++) {
-        if (request[j] > need[p][j]) {
-            printf("Error: Request exceeds process maximum claim.\n");
-            return 0;
-        }
-    }
-
-    for (int j = 0; j < m; j++) {
-        if (request[j] > available[j]) {
-            printf("Request cannot be granted immediately. Not enough resources.\n");
-            return 0;
-        }
-    }
-
-    for (int j = 0; j < m; j++) {
-        available[j] -= request[j];
-        allocation[p][j] += request[j];
-        need[p][j] -= request[j];
-    }
-
-    if (isSafe(n, allocation, need, available))
-        printf("Request granted. System remains SAFE.\n");
-    else {
-        for (int j = 0; j < m; j++) {
-            available[j] += request[j];
-            allocation[p][j] -= request[j];
-            need[p][j] += request[j];
-        }
-        printf("Request cannot be granted as system would be UNSAFE.\n");
-    }
+    if(detectDeadlock(n, allocation, request, available))
+        printf("System is in DEADLOCK.\n");
+    else
+        printf("System is NOT in deadlock.\n");
 
     return 0;
 }
+/*
+0 1 0
+2 0 0
+3 0 3
+2 1 1
+0 0 2
+*/
 
 /*
-0 0 0 
-2 0 2 
-0 0 0 
+0 0 0
+2 0 2
+0 0 0
 1 0 0 
 0 0 2
 */
